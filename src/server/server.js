ea6bd43442217c46a811e7627231e13c49282a47
Andrew <andykuo1supergreen@gmail.com>
2018-08-08 23:38:52 -0700

    Changed port for server.js

--- src/server/server.js
+++ src/server/server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
 
+app.set('port', 8080);
 app.get('/', (req, res) => {
 
 });
 
-app.listen(3000, function() {
-
+app.listen(app.get('port'), function() {
+  console.log("Server started.");
 });
