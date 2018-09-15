ecfc89312b6c1f3af65682cb6fcdfc62a10d4050
Andrew <andykuo1supergreen@gmail.com>
2018-09-15 09:44:19 -0700

    Moved unit cube data to geometry class

--- src/world/planehero/PlaneHero.js
+++ src/world/planehero/PlaneHero.js
@@ -199,7 +199,10 @@ class PlaneHero extends GameState
 }
 
 import Renderer from 'app/Renderer.js';
+
 import PlaneGeometry from 'render/mesh/PlaneGeometry.js';
+import UnitCubeGeometry from 'render/mesh/UnitCubeGeometry.js';
+
 import FreeLookCamera from 'render/camera/FreeLookCamera.js';
 import SceneGraphRenderer from 'render/SceneGraphRenderer.js';
 import AssetManifest from 'assets/pigeon/AssetManifest.js';
@@ -238,11 +241,14 @@ class PlaneHeroRenderer extends Renderer
     manifest.addAsset("image", "rock.jpg");
     manifest.addAsset("texture", "rock.tex", {image: "rock.jpg"});
 
-    manifest.addAsset("obj", "cube.obj");
+    //manifest.addAsset("obj", "cube.obj");
     manifest.addAsset("obj", "capsule.obj");
     manifest.addAsset("mesh", "capsule.mesh", {geometry: "capsule.obj"});
     manifest.addAsset("obj", "quad.obj");
     manifest.addAsset("mesh", "quad.mesh", {geometry: "quad.obj"});
+
+    assetManager.cacheAsset("obj", "cube.obj", new UnitCubeGeometry());
+    assetManager.loadAsset("mesh", "cube.mesh", {geometry: "cube.obj"});
 
     const planeObj = new PlaneGeometry(10, 10, 10, 10);
     assetManager.cacheAsset("obj", "plane.obj", planeObj);
@@ -263,16 +269,7 @@ class PlaneHeroRenderer extends Renderer
         mesh.texcoords.setData(texData);
       });
 
-    return super.load()
-    .then(() => assetManager.loadManifest(manifest))
-    .then(() => {
-      //Load mesh through cache
-      assetManager.cacheAsset("mesh", "cube.mesh", new Mesh(gl,
-        new Float32Array(defaultPositions),
-        new Float32Array(defaultTexcoords),
-        new Float32Array(defaultNormals),
-        new Uint16Array(defaultIndices)));
-    });
+    return super.load().then(() => assetManager.loadManifest(manifest));
   }
 
   //Override
@@ -293,118 +290,3 @@ class PlaneHeroRenderer extends Renderer
 }
 
 export default PlaneHero;
-
-const defaultPositions = [
-  // Front face
-  -1.0, -1.0,  1.0,
-   1.0, -1.0,  1.0,
-   1.0,  1.0,  1.0,
-  -1.0,  1.0,  1.0,
-
-  // Back face
-  -1.0, -1.0, -1.0,
-  -1.0,  1.0, -1.0,
-   1.0,  1.0, -1.0,
-   1.0, -1.0, -1.0,
-
-  // Top face
-  -1.0,  1.0, -1.0,
-  -1.0,  1.0,  1.0,
-   1.0,  1.0,  1.0,
-   1.0,  1.0, -1.0,
-
-  // Bottom face
-  -1.0, -1.0, -1.0,
-   1.0, -1.0, -1.0,
-   1.0, -1.0,  1.0,
-  -1.0, -1.0,  1.0,
-
-  // Right face
-   1.0, -1.0, -1.0,
-   1.0,  1.0, -1.0,
-   1.0,  1.0,  1.0,
-   1.0, -1.0,  1.0,
-
-  // Left face
-  -1.0, -1.0, -1.0,
-  -1.0, -1.0,  1.0,
-  -1.0,  1.0,  1.0,
-  -1.0,  1.0, -1.0,
-];
-const defaultTexcoords = [
-  // Front
-  0.0,  0.0,
-  1.0,  0.0,
-  1.0,  1.0,
-  0.0,  1.0,
-  // Back
-  0.0,  0.0,
-  1.0,  0.0,
-  1.0,  1.0,
-  0.0,  1.0,
-  // Top
-  0.0,  0.0,
-  1.0,  0.0,
-  1.0,  1.0,
-  0.0,  1.0,
-  // Bottom
-  0.0,  0.0,
-  1.0,  0.0,
-  1.0,  1.0,
-  0.0,  1.0,
-  // Right
-  0.0,  0.0,
-  1.0,  0.0,
-  1.0,  1.0,
-  0.0,  1.0,
-  // Left
-  0.0,  0.0,
-  1.0,  0.0,
-  1.0,  1.0,
-  0.0,  1.0,
-];
-const defaultNormals = [
-  // Front
-   0.0,  0.0,  1.0,
-   0.0,  0.0,  1.0,
-   0.0,  0.0,  1.0,
-   0.0,  0.0,  1.0,
-
-  // Back
-   0.0,  0.0, -1.0,
-   0.0,  0.0, -1.0,
-   0.0,  0.0, -1.0,
-   0.0,  0.0, -1.0,
-
-  // Top
-   0.0,  1.0,  0.0,
-   0.0,  1.0,  0.0,
-   0.0,  1.0,  0.0,
-   0.0,  1.0,  0.0,
-
-  // Bottom
-   0.0, -1.0,  0.0,
-   0.0, -1.0,  0.0,
-   0.0, -1.0,  0.0,
-   0.0, -1.0,  0.0,
-
-  // Right
-   1.0,  0.0,  0.0,
-   1.0,  0.0,  0.0,
-   1.0,  0.0,  0.0,
-   1.0,  0.0,  0.0,
-
-  // Left
-  -1.0,  0.0,  0.0,
-  -1.0,  0.0,  0.0,
-  -1.0,  0.0,  0.0,
-  -1.0,  0.0,  0.0
-];
-const defaultIndices = [
-  0,  1,  2,      0,  2,  3,    // front
-  4,  5,  6,      4,  6,  7,    // back
-  8,  9,  10,     8,  10, 11,   // top
-  12, 13, 14,     12, 14, 15,   // bottom
-  16, 17, 18,     16, 18, 19,   // right
-  20, 21, 22,     20, 22, 23,   // left
-];
